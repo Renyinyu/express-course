62b72488ae448120bed13cb0cfc6b5d8441d9f2d
chenjian <471630805@qq.com>
2022-12-19 22:44:20 +0800

    express learning

--- 02-express-tutorial/final/10-methods.js
+++ 02-express-tutorial/final/10-methods.js
@@ -10,8 +10,18 @@ app.use(express.urlencoded({ extended: false }))
 // 解析json
 app.use(express.json())
 
+app.get('/api/people', (req, res) => {
+  res.status(200).json({ success: true, data: people })
+})
 
+app.post('/api/people', (req, res) => {
+  const { name } = req.body
+  if (!name) {
+    return res.status(400).json({ success: false, msg: 'please provide name value' })
+  }
+  res.status(201).json({ success: true, person: name })
+})
 
 app.listen(5000, () => {
-  console.log('Server is listening on port 5000....')
+  console.log('')
 })
---
+++ 02-express-tutorial/routes/people.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const router = express.Router()
+
+const {
+  getPeople,
+  createPerson,
+  createPersonPostman,
+  updatePerson,
+  deletePerson,
+} = require('../controllers/people')
+
+router.route('/').get(getPeople).post(createPerson)
+
+router.route('/postman').post(createPersonPostman)
+
+router.route('/:id').put(updatePerson).delete(deletePerson)
+
+module.exports = router
